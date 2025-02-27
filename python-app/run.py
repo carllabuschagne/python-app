ef7c1b38001a8b7e369ffe9b58ca8bb847b63932
Carl Labuschagne <carl@ioxfleet.co.za>
2025-02-27 12:36:05 +0200

    update

--- python-app/run.py
+++ python-app/run.py
@@ -13,26 +13,26 @@ from config import config_dict
 from app import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 # The configuration
-get_config_mode = 'Debug' if DEBUG else 'Production'
+get_config_mode = "Debug" if DEBUG else "Production"
 
 try:
-    
-    # Load the configuration using the default values 
+
+    # Load the configuration using the default values
     app_config = config_dict[get_config_mode.capitalize()]
 
 except KeyError:
-    exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
+    exit("Error: Invalid <config_mode>. Expected values [Debug, Production] ")
 
-app = create_app( app_config ) 
+app = create_app(app_config)
 Migrate(app, db)
 
 if DEBUG:
-    app.logger.info('DEBUG       = ' + str(DEBUG)      )
-    app.logger.info('Environment = ' + get_config_mode )
-    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI )
+    app.logger.info("DEBUG       = " + str(DEBUG))
+    app.logger.info("Environment = " + get_config_mode)
+    app.logger.info("DBMS        = " + app_config.SQLALCHEMY_DATABASE_URI)
 
 if __name__ == "__main__":
     app.run()
